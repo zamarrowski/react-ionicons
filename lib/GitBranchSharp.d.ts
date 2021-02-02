f4580d7c5ef9e86a2e06fc973cbab865fd6ea102
Sergio Zamarro <sergio.zamarro.sanchez@bbva.com>
2021-02-02 20:51:53 +0100

    added typescript support

---
+++ lib/GitBranchSharp.d.ts
@@ -0,0 +1,33 @@
+export default GitBranchSharp;
+declare function GitBranchSharp(props: any): any;
+declare namespace GitBranchSharp {
+    namespace defaultProps {
+        const style: {};
+        const color: string;
+        const height: string;
+        const width: string;
+        const cssClasses: string;
+        const shake: boolean;
+        const beat: boolean;
+        const rotate: boolean;
+    }
+    namespace propTypes {
+        const style_1: any;
+        export { style_1 as style };
+        const color_1: any;
+        export { color_1 as color };
+        const height_1: any;
+        export { height_1 as height };
+        const width_1: any;
+        export { width_1 as width };
+        const cssClasses_1: any;
+        export { cssClasses_1 as cssClasses };
+        const shake_1: any;
+        export { shake_1 as shake };
+        const beat_1: any;
+        export { beat_1 as beat };
+        const rotate_1: any;
+        export { rotate_1 as rotate };
+        export const onClick: any;
+    }
+}
